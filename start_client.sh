7f44f83b545036dcd3212ef37c7c15fed1d3948d
vito.he <hxmhlt@163.com>
2019-05-08 14:52:54 +0800

    Add:client and server in different machine

--- start_client.sh
+++ start_client.sh
@@ -1,6 +1,6 @@
 
-#c 代表压测并发数，n代表压测总数，p代表协议
-while getopts ":p:n:c:" opt
+#c 代表压测并发数，n代表压测总数
+while getopts ":n:c:" opt
 do
     case $opt in
         c)
@@ -9,16 +9,13 @@ do
         n)
         nopt=$OPTARG
         ;;
-        p)
-        popt=$OPTARG
-        ;;
         ?)
         echo "未知参数"
         exit 1;;
     esac
 done
 
-#判断 p、c和n参数是否输入，若未输入，则默认dubbo,1,1
+#判断 c和n参数是否输入，若未输入，则默认dubbo,1,1
 if  [ ! -n "$copt" ] ;then
     copt=1
 fi
@@ -27,22 +24,11 @@ if  [ ! -n "$nopt" ] ;then
     nopt=1
 fi
 
-if  [ ! -n "$popt" ] ;then
-    popt="dubbo"
-fi
-
-sh stop.sh
-export CONF_PROVIDER_FILE_PATH=$PWD/$popt/server.yml
-export APP_LOG_CONF_FILE=$PWD/$popt/log.xml
-
-go run ./ &
-echo "进程ID:"$!
-echo $!>./pid
-sleep 4
 export -n CONF_PROVIDER_FILE_PATH
 export CONF_CONSUMER_FILE_PATH=$PWD/$popt/client/client.yml
 export APP_LOG_CONF_FILE=$PWD/$popt/client/log.xml
 
+
 go run ./$popt/client/*.go -c $copt  -n $nopt
-sh stop.sh
+
 
---
+++ start_server.sh
@@ -0,0 +1,29 @@
+
+#p代表协议
+while getopts ":p:" opt
+do
+    case $opt in
+        p)
+        popt=$OPTARG
+        ;;
+        ?)
+        echo "未知参数"
+        exit 1;;
+    esac
+done
+
+#判断 p参数是否输入，若未输入，则默认dubbo
+
+if  [ ! -n "$popt" ] ;then
+    popt="dubbo"
+fi
+
+sh stop.sh
+export -n CONF_CONSUMER_FILE_PATH
+export CONF_PROVIDER_FILE_PATH=$PWD/$popt/server.yml
+export APP_LOG_CONF_FILE=$PWD/$popt/log.xml
+
+go run ./ &
+echo "进程ID:"$!
+echo $!>./pid
+
